8bcde9ac359238ff9ed1bb17f1a3c30ffd8fc8ab
Frank <f.neumann.berlin@googlemail.com>
2023-02-08 12:29:59 +0100

    Initial commit

---
+++ app/src/main/java/com/example/filedemo/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.filedemo
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.filedemo.databinding.ActivityMainBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.net.FileNameMap
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private val TAG = "FileDemo"
+    private val FileNameMap = "$TAG.txt"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Log.d(TAG, "filesDir: $filesDir ")
+        createFile(filesDir,"A")
+        val dirAudio = getDir("audio",Context.MODE_PRIVATE)
+        val dirOrte = getDir("orte", MODE_PRIVATE)
+        createFile(dirAudio,"B")
+        createFile(dirOrte,"Berlin")
+
+        File.createTempFile("MyTmp", "tmp",cacheDir)
+        File.createTempFile("MyTmpD", "tmp",cacheDir).deleteOnExit()
+
+    }
+
+    private fun createFile(path: File, file:String){
+        val fullPath = File(path,file)
+        FileOutputStream(fullPath).use { fos ->
+            fos.write("Hallo".toByteArray())
+        }
+    }
+}
